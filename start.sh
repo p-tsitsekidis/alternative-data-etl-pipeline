b93d0000011a38d529ab7aff94c11dae1a3e995f
Petros <p.tsitsekidis@gmail.com>
2026-05-18 12:43:11 +0200

    Modify to 1 worker 4 threads

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ echo "Starting Flask API in the background..."
 
 # Use the virtual environment's gunicorn and bind to 0.0.0.0.
 # Send all output to a log file.
-/app/venv/bin/gunicorn -w 2 --threads 2 -k gthread --timeout 120 -b 0.0.0.0:8000 flask_api:app > /app/api.log 2>&1 &
+/app/venv/bin/gunicorn -w 1 --threads 4 -k gthread --timeout 120 -b 0.0.0.0:8000 flask_api:app > /app/api.log 2>&1 &
 
 # Run a background process to constantly print the API logs to Render's console
 tail -f /app/api.log &
